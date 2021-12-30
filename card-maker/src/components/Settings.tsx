9960237d7aaaed46822ef73cad4ad11c99657700
CottonBottom <87255233+CottonBottom@users.noreply.github.com>
2021-12-30 10:05:19 +0900

    Adjusting Settings
    Adjusting Settings

--- card-maker/src/components/Settings.tsx
+++ card-maker/src/components/Settings.tsx
@@ -3,7 +3,19 @@ import React from "react";
 type Props = {};
 
 const Settings: React.FC<Props> = (props: Props) => {
-  return <div className="card card--settings">My Settings</div>;
+  return (
+    <div className="card card--settings">
+      <div className="tab-container">
+        <div className="tabs">
+          <button className="tab-button">Tab1</button>
+          <button className="tab-button">Tab2</button>
+          <button className="tab-button">Tab3</button>
+        </div>
+      </div>
+      <div className="tab-content">Tab Content</div>
+      <button className="button button--full-width">Generate</button>
+    </div>
+  );
 };
 
 export default Settings;
