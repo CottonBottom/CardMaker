ce3c9b3c05bbb715f8700ada8d9eeb88a231b269
CottonBottom <87255233+CottonBottom@users.noreply.github.com>
2022-01-01 13:18:36 +0900

    Card Design Done

--- card-maker/src/components/Card.tsx
+++ card-maker/src/components/Card.tsx
@@ -4,26 +4,32 @@ type Props = {};
 
 const Card: React.FC<Props> = (props: Props) => {
   return (
-    <div className="card">
+    <div className="card card--character-card">
       <div className="character-card-container">
         <div className="character-card">
-          <div className="character-card__top">Hello</div>
+          <div className="character-card__top">
+            <span className="character-card__cost">
+              <span>20</span>
+            </span>
+            <span className="material-icons">pets</span>
+          </div>
           <div className="character-card__image-container">
             <div className="character-card__name">
               Card Name Very Long To Test
             </div>
           </div>
           <div className="character-card__legend">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam,
+            </span>
           </div>
           <div className="character-card__abilities">
             <div className="character-card__skills">
-              <span>Some Very Long Skill Going Two Lines Too</span>
-              <span>Some Very Long Skill Going Two Lines Too</span>
-              <span>Some Very Long Skill Going Two Lines Too</span>
+              <span>Very Long Skill</span>
+              <span>Very Long Skill</span>
+              <span>Very Long Skill</span>
             </div>
             <div className="character-card__stats">3/3</div>
           </div>
